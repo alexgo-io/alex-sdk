2133f2f3f29d86561da793c34ea4f00bd5fed10c
Kyle Fang <zhigang1992@gmail.com>
2024-04-14 23:00:33 +0800

    fix: post condition on LQSTX

--- src/utils/postConditions.ts
+++ src/utils/postConditions.ts
@@ -18,6 +18,13 @@ export function transfer(
   compare: FungibleConditionCode = FungibleConditionCode.Equal
 ): FungiblePostCondition | STXPostCondition {
   const scale = getCurrencyNativeScale(currency);
+  if (currency === Currency.LQSTX) {
+    // LQSTX is a rebase token, the ft-event number will not match
+    // The amount inputted. So we set the amount to 0, and the compare
+    // to GreaterEqual to ensure the condition passes.
+    amount = BigInt(0);
+    compare = FungibleConditionCode.GreaterEqual;
+  }
   const nativeAmount = (amount * BigInt(scale)) / BigInt(1e8);
   if (currency === Currency.STX) {
     return createSTXPostCondition(senderAddress, compare, nativeAmount);
